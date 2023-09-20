895f2012f503f1e36ff1ea104cb2588ce225e5ba
Luan053 <luanrschulzz@gmail.com>
2023-09-20 12:43:58 -0300

    Adicionando Interface

--- BooksAPI/Repositories/IBookRepository.cs
+++ BooksAPI/Repositories/IBookRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace BooksAPI.Repositories
+﻿using BooksAPI.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAPI.Repositories
 {
     public interface IBookRepository
     {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int id);
+
     }
 }
--- BooksAPI/Startup.cs
+++ BooksAPI/Startup.cs
@@ -1,8 +1,10 @@
 using BooksAPI.Model;
+using BooksAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,8 @@ namespace BooksAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BookContext>;
+            IServiceCollection serviceCollection = services.AddDbContext<BookContext>(x => x.UseSqlite("Data source=books.db"));
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
